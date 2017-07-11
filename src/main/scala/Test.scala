b3748edeea1975250e45fdbbd72c3d82b174dcfa
David Portabella <david.portabella@gmail.com>
2017-07-11 18:32:12 +0200

    added locality and country

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -9,7 +9,7 @@ case class Result(address_components: List[AddressComponent], geometry: Geometry
 case class Response(results: List[Result], status: String)
 
 // output
-case class Address(text: String, exactMath: Boolean, areaLevel1: Option[String], areaLevel2: Option[String], areaLevel3: Option[String], postalCode: Option[String], location: Option[Location])
+case class Address(text: String, exactMath: Boolean, locality: Option[String], areaLevel1: Option[String], areaLevel2: Option[String], areaLevel3: Option[String], postalCode: Option[String], country: Option[String], location: Option[Location])
 
 object Test {
   implicit val addressComponentFormats = Json.format[AddressComponent]
@@ -27,13 +27,15 @@ object Test {
     val exactMath = response.results.length == 1 && response.status == "OK"
 
     response.results.headOption.map { result =>
+      val locality = result.address_components.find(_.types.contains("locality")).map(_.long_name)
       val areaLevel1 = result.address_components.find(_.types.contains("administrative_area_level_1")).map(_.long_name)
       val areaLevel2 = result.address_components.find(_.types.contains("administrative_area_level_2")).map(_.long_name)
       val areaLevel3 = result.address_components.find(_.types.contains("administrative_area_level_3")).map(_.long_name)
       val postalCode = result.address_components.find(_.types.contains("postal_code")).map(_.long_name)
+      val country = result.address_components.find(_.types.contains("country")).map(_.short_name)
       val location = result.geometry.location
 
-      Address(address, exactMath, areaLevel1, areaLevel2, areaLevel3, postalCode, location)
+      Address(address, exactMath, locality, areaLevel1, areaLevel2, areaLevel3, postalCode, country, location)
     }
   }
 
