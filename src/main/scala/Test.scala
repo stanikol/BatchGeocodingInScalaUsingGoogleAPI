76d5ff6a6d80e124098ad4725312cb2f6d3253aa
David Portabella <david.portabella@gmail.com>
2017-07-11 18:28:53 +0200

    define option values

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,14 +1,15 @@
 import play.api.libs.json._
 import java.net.URLEncoder
 
+// input
 case class AddressComponent(long_name: String, short_name: String, types: List[String])
 case class Location(lat: Double, lng: Double)
-case class Geometry(location: Location, location_type: String)
+case class Geometry(location: Option[Location], location_type: String)
 case class Result(address_components: List[AddressComponent], geometry: Geometry, formatted_address: String, place_id: String, types: List[String])
 case class Response(results: List[Result], status: String)
 
-
-case class Address(text: String, areaLevel1: String, areaLevel2: String, areaLevel3: String, postalCode: String, location: Location)
+// output
+case class Address(text: String, exactMath: Boolean, areaLevel1: Option[String], areaLevel2: Option[String], areaLevel3: Option[String], postalCode: Option[String], location: Option[Location])
 
 object Test {
   implicit val addressComponentFormats = Json.format[AddressComponent]
@@ -18,18 +19,22 @@ object Test {
   implicit val ResponseFormats = Json.format[Response]
 
 
-  def parseAddress(address: String): Address = {
+  def parseAddress(address: String): Option[Address] = {
     val url = s"https://maps.googleapis.com/maps/api/geocode/json?address=${URLEncoder.encode(address, "UTF-8")}&key=AIzaSyAOcEY_1Y6q47MN-7SlZL8xQuHeIgwVaY8"
     val text = new String(Utils.download(url))
     val response = Json.parse(text).validate[Response].get
 
-    val areaLevel1 = response.results(0).address_components.filter(_.types.contains("administrative_area_level_1")).apply(0).long_name
-    val areaLevel2 = response.results(0).address_components.filter(_.types.contains("administrative_area_level_2")).apply(0).long_name
-    val areaLevel3 = "" //response.results(0).address_components.filter(_.types.contains("administrative_area_level_1")).apply(0).long_name
-    val postalCode = response.results(0).address_components.filter(_.types.contains("postal_code")).apply(0).long_name
-    val location = response.results(0).geometry.location
+    val exactMath = response.results.length == 1 && response.status == "OK"
 
-    Address(address, areaLevel1, areaLevel2, areaLevel3, postalCode, location)
+    response.results.headOption.map { result =>
+      val areaLevel1 = result.address_components.find(_.types.contains("administrative_area_level_1")).map(_.long_name)
+      val areaLevel2 = result.address_components.find(_.types.contains("administrative_area_level_2")).map(_.long_name)
+      val areaLevel3 = result.address_components.find(_.types.contains("administrative_area_level_3")).map(_.long_name)
+      val postalCode = result.address_components.find(_.types.contains("postal_code")).map(_.long_name)
+      val location = result.geometry.location
+
+      Address(address, exactMath, areaLevel1, areaLevel2, areaLevel3, postalCode, location)
+    }
   }
 
   def main(args: Array[String]) {
