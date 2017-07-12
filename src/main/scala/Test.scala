8a86588c40a0d36df2fc463828bf874b14ccbb53
David Portabella <david.portabella@gmail.com>
2017-07-12 17:11:56 +0200

    small refactoring, mostly variable names

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -7,26 +7,15 @@ import anorm.{SqlParser, _}
 import scala.util.Try
 
 
-// input
+// google json response
 case class AddressComponent(long_name: String, short_name: String, types: List[String])
-case class Location(lat: Double, lng: Double)
+case class Location(lat: Float, lng: Float)
 case class Geometry(location: Option[Location], location_type: String)
 case class Result(address_components: List[AddressComponent], geometry: Geometry, formatted_address: String, place_id: String, types: List[String])
 case class Response(results: List[Result], status: String)
 
-// output
-case class Address(text: String, response: String, exactMath: Boolean, locality: Option[String], areaLevel1: Option[String], areaLevel2: Option[String], areaLevel3: Option[String], postalCode: Option[String], country: Option[String], location: Option[Location])
-
-
-/*
-dbUrl=jdbc:mysql://cdm6-143.epfl.ch/patstat_2015a?user=mysqluser&password=__PASSWORD__&useSSL=false&useUnicode=yes&characterEncoding=utf8
-dbUrl=jdbc:postgresql://localhost/test?user=fred&password=__PASSWORD__&ssl=true
-
-sql:
-create table addresses(text varchar(500) unique, response text, exactMatch int, locality varchar(255), areaLevel1 varchar(255), areaLevel2 varchar(255), areaLevel3 varchar(255), postalCode varchar(100), country varchar(100), lat decimal, lng decimal, index(exactMatch), index(locality), index(areaLevel1), index(areaLevel2), index(areaLevel3), index(postalCode), index(country), index(lat), index(lng))
-insert into addresses (text) select distinct(concat(address_freeform, ', ', person_ctry_code)) from TLS226_PERSON_ORIG;
-select text, exactMatch, locality, areaLevel1, areaLevel2, areaLevel3, postalCode, country, lat, lng from addresses where response is not null;
-*/
+// information extracted
+case class Address(addressToQuery: String, googleResponse: String, exactMath: Boolean, locality: Option[String], areaLevel1: Option[String], areaLevel2: Option[String], areaLevel3: Option[String], postalCode: Option[String], country: Option[String], location: Option[Location], formattedAddress: String)
 
 object Test {
   implicit val addressComponentFormats = Json.format[AddressComponent]
@@ -44,25 +33,27 @@ object Test {
     val exactMath = response.results.length == 1 && response.status == "OK"
 
     response.results.headOption.map { result =>
-      val locality = result.address_components.find(_.types.contains("locality")).map(_.long_name)
-      val areaLevel1 = result.address_components.find(_.types.contains("administrative_area_level_1")).map(_.long_name)
-      val areaLevel2 = result.address_components.find(_.types.contains("administrative_area_level_2")).map(_.long_name)
-      val areaLevel3 = result.address_components.find(_.types.contains("administrative_area_level_3")).map(_.long_name)
-      val postalCode = result.address_components.find(_.types.contains("postal_code")).map(_.long_name)
-      val country = result.address_components.find(_.types.contains("country")).map(_.short_name)
+      def addressComponent(typeId: String) = result.address_components.find(_.types.contains(typeId))
+      val locality = addressComponent("locality").map(_.long_name)
+      val areaLevel1 = addressComponent("administrative_area_level_1").map(_.long_name)
+      val areaLevel2 = addressComponent("administrative_area_level_2").map(_.long_name)
+      val areaLevel3 = addressComponent("administrative_area_level_3").map(_.long_name)
+      val postalCode = addressComponent("postal_code").map(_.long_name)
+      val country = addressComponent("country").map(_.short_name)
       val location = result.geometry.location
+      val formattedAddress = result.formatted_address
 
-      Address(address, responseText, exactMath, locality, areaLevel1, areaLevel2, areaLevel3, postalCode, country, location)
+      Address(address, responseText, exactMath, locality, areaLevel1, areaLevel2, areaLevel3, postalCode, country, location, formattedAddress)
     }
   }
 
-  def parseAddressAndSaveToDatabase(addressText: String)(implicit conn: Connection) {
-    println(s"parseAddressAndSaveToDatabase: $addressText")
-    parseAddress(addressText).foreach { address =>
-      import address._
-      val r: Int = SQL"update addresses set response=$response, exactMatch=$exactMath, locality=$locality, areaLevel1=$areaLevel1, areaLevel2=$areaLevel2, areaLevel3=$areaLevel3, postalCode=$postalCode, country=$country, lat=${location.map(_.lat)}, lng=${location.map(_.lng)} where text=$addressText"
+  def parseAddressAndSaveToDatabase(address: String)(implicit conn: Connection) {
+    println(s"parseAddressAndSaveToDatabase: $address")
+    parseAddress(address).foreach { parsedAddress =>
+      import parsedAddress._
+      val r: Int = SQL"update addresses set googleResponse=$googleResponse, exactMatch=$exactMath, locality=$locality, areaLevel1=$areaLevel1, areaLevel2=$areaLevel2, areaLevel3=$areaLevel3, postalCode=$postalCode, country=$country, lat=${location.map(_.lat)}, lng=${location.map(_.lng)}, formattedAddress=$formattedAddress where addressToQuery=$address"
         .executeUpdate()
-      if (r != 1) println(s"error on $addressText")
+      if (r != 1) println(s"error on $address")
     }
   }
 
@@ -71,7 +62,7 @@ object Test {
 
     implicit val conn = Utils.getDbConnection
     val addresses: List[String] =
-      SQL"select text from addresses where response is null limit 1000".as(SqlParser.str(1).*)
+      SQL"select addressToQuery from addresses where googleResponse is null limit 1000".as(SqlParser.str(1).*)
 
     addresses.foreach(a => Try(parseAddressAndSaveToDatabase(a)))
 
