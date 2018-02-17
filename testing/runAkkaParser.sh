078194ccf4092ed6eb0a33697b05eaa0427a88c1
Stanislav Nikolnikov <stanikol@gmail.com>
2018-02-17 23:23:53 +0200

    GoogleApiFlow improoving

--- testing/runAkkaParser.sh
+++ testing/runAkkaParser.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-export $googleApiKey=googleApiKey
+export googleApiKey=googleApiKey
+#export googleApiKey="AIzaSyAl3u33Ea4Nw31iVKP5uPE4KfwW-vnXawc"
 export dbUrl="jdbc:mysql://localhost/test?user=root&useSSL=false&useUnicode=yes&characterEncoding=utf8"
 
 #--op=googleQueryAndParse
 #--op=parseOnly
 #--op=googleQueryOnly
 sbt "runMain akka_parser.BatchParserCmd2
---op=googleQueryOnly
+--op=googleQueryAndParse
 --maxEntries=1000000
 --maxGoogleAPIOpenRequests=32
 --maxGoogleAPIFatalErrors=5
